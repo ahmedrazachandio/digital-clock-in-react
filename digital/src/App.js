f30196b9e2e3ff9eec920ea279f842c5c48d42a9
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-19 00:28:30 +0500

    finally run digital clock

--- digital/src/App.js
+++ digital/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
+
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
   let time = new Date().toLocaleTimeString();
 
+  const [ctime, seCtime] = useState(time);
+
 
   const UpdateTime = () => {
     time = new Date().toLocaleTimeString();
+    seCtime(time);
   }
   setInterval(UpdateTime, 1000);
 
