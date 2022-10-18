a557321261ddddd863bee67e3c63b500b866c508
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-19 00:20:25 +0500

    create function update time

--- digital/src/App.js
+++ digital/src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+  }
+
   return (
     <div className="App">
+      <h1>{time}</h1>
       
     </div>
   );
