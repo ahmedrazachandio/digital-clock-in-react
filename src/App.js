a564380bfc5d9c0b076e8e5ecc9d8f94e7de5713
Mairajk <86877851+ahmedrazachandio@users.noreply.github.com>
2022-10-19 21:52:15 +0500

    styling upto date

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 // import logo from './logo.svg';
 
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   let time = new Date().toLocaleTimeString();
 
   const [ctime, seCtime] = useState(time);
 
-
   const UpdateTime = () => {
     time = new Date().toLocaleTimeString();
     seCtime(time);
-  }
+  };
   setInterval(UpdateTime, 1000);
 
   return (
-    <div className="App  timer">
-      <div className="card"> 
-
-        <h1 className="time">{time}</h1>
+    <div className="App">
+      <div className="container">
+        <div className="card">
+          <h1 className="time">{time}</h1>
+        </div>
       </div>
-      <a href="https://ahmedrazachandio.github.io/" target="_blank">Ahmed Raza</a>
+      
+      
+      <div className="footer">
+        <a href="https://ahmedrazachandio.github.io/" target="_blank">
+          Powered and Developed By Ahmed Raza
+        </a>
+      </div>
     </div>
   );
 }
